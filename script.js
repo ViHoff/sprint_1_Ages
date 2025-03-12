d1d30cd1744dec3c334ac41f05abf017771471a5
ViHoff <vice.hof@hotmail.com>
2025-03-12 16:53:54 -0300

    commit segundo dia de Sprint

---
+++ script.js
@@ -0,0 +1,31 @@
+const screen = document.getElementById("screen");
+screen.value = "0";
+
+function showScreen(input)
+{
+    if(screen.value==0)
+    {
+        screen.value="";
+        screen.value += input;
+    }
+    else
+    {
+        screen.value += input;
+    }
+}
+
+function clearScreen()
+{
+    screen.value = "0";
+}
+
+function calculate()
+{
+    screen.value = eval(screen.value);
+}
+
+function invertNumber()
+{
+    screen.value = (screen.value*-1);
+}
+
